f792088bb12cec4d0f3476c47c0c23de9f8b328b
Wayne <107938367+ckd4@users.noreply.github.com>
2023-04-04 02:58:27 +0300

    Add files via upload

--- chatgpt.py
+++ chatgpt.py
@@ -10,7 +10,7 @@ from aiogram import Bot, types
 
 bot = telebot.TeleBot('6002185430:AAH9Sy1z2rHStVumXNGDA_hBre_OXEOhLtw')
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 @bot.message_handler(commands=['start'])
 def main(message):
