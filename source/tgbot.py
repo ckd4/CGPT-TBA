120c2aca373e6b0674c03a3e71f3f22907654379
FUMOBOMBER <ah3082340@gmail.com>
2023-04-17 00:36:50 +0300

    checkpoint

--- source/tgbot.py
+++ source/tgbot.py
@@ -7,10 +7,8 @@ def start() -> None:
 	load_dotenv()
 	logger.debug("READING ENVIRONMENT VARIABLES")
 	bot_token = os.getenv("TELEGR_TOKEN")
-	oai_token = os.getenv("OPENAI_TOKEN")
-	logger.info("BOTT CONFIGURATION")
+	logger.info("BOT CONFIGURATION")
 	logger.debug(f"TELEGR_TOKEN={bot_token}")
-	logger.debug(f"OPENAI_TOKEN={oai_token}")
 	logger.info("BOT OBJECT INITIALISATION")
 	bot = telebot.TeleBot(bot_token)
 	logger.debug(bot)
@@ -18,15 +16,12 @@ def start() -> None:
 	@bot.message_handler(commands=["start"])
 	def msg_start(message):
 		logger.info(f"{message.chat.id} -> msg:'/start'")
-		bot.send_message(message.chat.id, "Hello")
+		bot.send_message(message.chat.id, "[ start prompt here ]")
 
-	@bot.message_handler(commands=["image"])
+	@bot.message_handler(commands=["ask"])
 	def msg_image(message):
-		logger.info(f"{message.chat.id} -> msg:'/image'")
-		if (os.path.exists("source\content\image.gif") == False):
-			logger.warning("UNABLE TO FIND IMAGE FILE")
-			bot.send_massage(message.chat.id, "Unable to load image :(")
-		else:
-			bot.send_photo(message.chat.id, photo=open("source\content\image.gif", "rb"))
+		logger.info(f"{message.chat.id} -> msg:'/ask'")
+		bot.send_message(message.chat.id, "[ chatgpt response here ]")
+
 
 	bot.polling()
